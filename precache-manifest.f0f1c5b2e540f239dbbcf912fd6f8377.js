13abb92ab53c1015513bd082b314f0acd499fde4
pawel <bareja.pawel@gmail.com>
2020-04-01 17:12:08 +0200

    Updates

---
+++ precache-manifest.f0f1c5b2e540f239dbbcf912fd6f8377.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b15ef172e615515c18cb0dd2d78f263",
+    "url": "/pawelBareja/survey.git/index.html"
+  },
+  {
+    "revision": "12a16f8c6db2c1a4aa8e",
+    "url": "/pawelBareja/survey.git/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "5f8655a7014df3c5fa63",
+    "url": "/pawelBareja/survey.git/static/js/2.6c93852f.chunk.js"
+  },
+  {
+    "revision": "12a16f8c6db2c1a4aa8e",
+    "url": "/pawelBareja/survey.git/static/js/main.9a8fa2b4.chunk.js"
+  },
+  {
+    "revision": "55c9e7d52f561314c2fa",
+    "url": "/pawelBareja/survey.git/static/js/runtime-main.35437b48.js"
+  }
+]);
